2f2c089c1f3aad06f38757c84882a5b3bfcc13f7
xzz-233 <xuzhouzi@qq.com>
2019-03-25 22:01:11 +0800

    重构呆码*2

--- orzyyb.js
+++ orzyyb.js
@@ -35,18 +35,19 @@ yyb又变强了！\
 var orzCount=0,orzDelta=1,says2_restTime=0;
 var sayslist=new Array("你个菜逼我今天AK了","NOI怎么这么水啊，这不是用来随手AK的吗","ZSY你个菜逼虽然我要和你在一起但是你还是好菜啊","你怎么还在乱膜啊？？？你快吸掉我RP的0.000000001‰了","今天考试又是三个傻逼题，我不仅AK了还把2048玩到了2^1000000","怎么我出的题全场爆零啊，我以为会有人负分的啊","你们还是naive，我怎么可能不AK？","今天题真的水，你们不AK的都退役吧");
 function ORZINF() {
+	var says2_restTime_Default=50;
     orzCount+=orzDelta;
     document.getElementById("orzTimes").innerHTML='OrzYYB!*'+orzCount;
     //document.getElementById("yybsays").innerHTML='YYB：'+sayslist[Math.floor(Math.random()*sayslist.length)];
     --says2_restTime;
     if(says2_restTime==0)document.getElementById("yybsays2").innerHTML="";
-    if(orzCount==1)document.getElementById("yybsays2").innerHTML='YYB：诶又有一个来膜拜我的，来吧来吧继续膜',says2_restTime=5;
-    if(orzCount==2)document.getElementById("yybsays2").innerHTML='YYB：你们还是naive，我又AK了',says2_restTime=5;
-    if(orzCount==10)document.getElementById("yybsays2").innerHTML='YYB：虽然我是大佬，但是你这样一直膜我是会掉RP的',says2_restTime=5;
-    if(orzCount==20)document.getElementById("yybsays2").innerHTML='YYB：MDZZ你怎么还在膜信不信我把你从5楼扔下去',says2_restTime=5;
-    if(orzCount==50)document.getElementById("yybsays2").innerHTML='YYB：没错我就是这么强，让你好好膜',says2_restTime=5;
+    if(orzCount==1)document.getElementById("yybsays2").innerHTML='YYB：诶又有一个来膜拜我的，来吧来吧继续膜',says2_restTime=says2_restTime_Default;
+    if(orzCount==2)document.getElementById("yybsays2").innerHTML='YYB：你们还是naive，我又AK了',says2_restTime=says2_restTime_Default;
+    if(orzCount==10)document.getElementById("yybsays2").innerHTML='YYB：虽然我是大佬，但是你这样一直膜我是会掉RP的',says2_restTime=says2_restTime_Default;
+    if(orzCount==20)document.getElementById("yybsays2").innerHTML='YYB：MDZZ你怎么还在膜信不信我把你从5楼扔下去',says2_restTime=says2_restTime_Default;
+    if(orzCount==50)document.getElementById("yybsays2").innerHTML='YYB：没错我就是这么强，让你好好膜',says2_restTime=says2_restTime_Default;
     if(orzCount==50)orzDelta=5,document.getElementById("orzbutton").innerHTML="继续膜拜"+orzDelta+"次";
-    if(orzCount==1000)document.getElementById("yybsays2").innerHTML='YYB：你真棒，我感受到了我又变强了，让你一次多膜一点',says2_restTime=5;
+    if(orzCount==1000)document.getElementById("yybsays2").innerHTML='YYB：你真棒，我感受到了我又变强了，让你一次多膜一点',says2_restTime=says2_restTime_Default;
     if(orzCount>=1000) {
         orzDelta=Math.floor(Math.random()*100+5);
         document.getElementById("orzbutton").innerHTML="继续膜拜"+orzDelta+"次";
